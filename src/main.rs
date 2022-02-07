481f8b8497ec0cb184dd50cc6119e94774496b94
Titan <68808479+titan3755@users.noreply.github.com>
2022-02-07 14:23:45 +0600

    Appearance Changes
    Changed the console appearances in the app.

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,37 @@
 extern crate colour;
 extern crate regex;
 extern crate colored;
+extern crate term_table;
+extern crate figlet_rs;
 
 use std::fs;
-use std::io;
-use std::io::prelude::*;
 use regex::Regex;
 use colored::Colorize;
+use figlet_rs::FIGfont;
+use term_table::{Table, row::Row, TableStyle, table_cell::{Alignment, TableCell}};
 
 fn main() {
-    let mut path = String::new();
-    let mut query = String::new();
-    green_ln!("Welcome to the string search program!");
-    green!("Please enter the absolute path to the file you wish to search: ");
-    std::io::stdin().read_line(&mut path).expect("Failed to read line");
-    green!("Please enter the string you wish to search for: ");
-    std::io::stdin().read_line(&mut query).expect("Failed to read line");
-    path = path.trim().to_string();
-    query = query.trim().to_string();
-    if path.trim().is_empty() || query.trim().is_empty() {
-        red_ln!("Filepath and/or query cannot be empty!");
-        pause();
-    }
-    else {
-        yellow_ln!("Searching for {} in {} ...\n", query, path);
-        string_searcher_main(&query, &path);
-        print!("\nSearch complete! ");
-        pause();
+    loop {
+        let mut path = String::new();
+        let mut query = String::new();
+        let standard_font = FIGfont::standand().unwrap();
+        let figure = standard_font.convert("TEXT - SEARCHER").unwrap();
+        blue_ln!("{}", figure);
+        green!("Please enter the absolute path to the file you wish to search: ");
+        std::io::stdin().read_line(&mut path).expect("Failed to read line");
+        green!("Please enter the string you wish to search for: ");
+        std::io::stdin().read_line(&mut query).expect("Failed to read line");
+        path = path.trim().to_string();
+        query = query.trim().to_string();
+        if path.trim().is_empty() || query.trim().is_empty() {
+            red_ln!("Filepath and/or query cannot be empty!");
+            pause("\nRetry? (y/n): ");
+        }
+        else {
+            yellow_ln!("Searching for {} in {} ...\n", query, path);
+            string_searcher_main(&query, &path);
+            pause("\nSearch again? (y/n): ");
+        }
     }
 }
 
@@ -39,7 +44,7 @@ fn string_searcher_main(query: &String, file_path: &String) {
             return;
         }
     }.trim().to_string();
-    println!("-----------------------------------------------------");
+    table_creator("Search Start");
     let mut count = 0;
     let re = Regex::new(&format!(r"({})", query.trim())[..]).unwrap();
     for (i, _) in re.captures_iter(&data).enumerate() {
@@ -47,16 +52,15 @@ fn string_searcher_main(query: &String, file_path: &String) {
         cyan_ln!("Found \"{}\" in {} [{}]", query, file_path, i);
     }
     if count != 0 {
-        green_ln!("Found {} occurrence(s) of \"{}\" in {}", count, query, file_path);
+        green_ln!("Found {} occurrence(s) of \"{}\" in {}\n", count, query, file_path);
     }
     else {
         red_ln!("Could not find \"{}\" in {}", query, file_path);
     }
-    println!("-----------------------------------------------------");
+    table_creator("Search Completed");
     let highlighted_data = highlight_string(&String::from(data), &query);
-    green_ln!("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    table_creator("Highlighted Search Results");
     println!("{}", highlighted_data);
-    green_ln!("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 }
 
 fn highlight_string(data: &String, query: &String) -> String {
@@ -72,11 +76,26 @@ fn highlight_string(data: &String, query: &String) -> String {
     highlighted_data
 }
 
-fn pause() {
-    let mut stdin = io::stdin();
-    let mut stdout = io::stdout();
-    write!(stdout, "Press enter to exit ...").unwrap();
-    stdout.flush().unwrap();
-    let _ = stdin.read(&mut [0u8]).unwrap();
+fn table_creator(text: &str) {
+    let mut table = Table::new();
+    table.max_column_width = 100;
+    table.style = TableStyle::extended(); 
+    table.add_row(Row::new(vec![
+        TableCell::new_with_alignment(text, 2, Alignment::Center)
+    ]));
+    println!("{}", table.render());
+}
+
+fn pause(phrase: &str) {
+    let mut input = String::new();
+    green!(phrase);
+    std::io::stdin().read_line(&mut input).expect("Failed to read line");
+    if input.trim().to_lowercase() == "y" {
+        return;
+    }
+    else {
+        red_ln!("Exiting program...");
+        std::process::exit(0);
+    }
 }
 
